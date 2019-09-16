8126b8e24cdd31f2fe4a64d6850c40fc1d06e31b
Sarkisyan <azsarki@USBUR12WB162522s-MacBook-Pro.local>
2019-09-15 20:44:48 -0700

    fixing critical bugs last time, making sure types align

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine
+import sqlalchemy
 import json
 import pandas as pd
 import boto3
@@ -22,7 +22,7 @@ def fetch_secrets():
 
 def generate_psql_conn():
   creds = fetch_secrets()
-  engine = create_engine(
+  engine = sqlalchemy.create_engine(
     'postgresql://{user}:{password}@{host}:{port}/{dbname}'.format(
         user=creds['username'],
         password=creds['password'],
@@ -56,33 +56,71 @@ def cleanup_event_date(event_date):
   except:
     return None
 
+def cleanup_headers(headers_dict):
+  headers = {k.lower():j for k,j in dict(headers_dict).items()}
+  for h in ['date','eventdate']:
+    if h in headers:
+      headers[h] = pd.to_datetime(cleanup_event_date(headers[h]), errors='coerce').date().strftime('%Y%m%d')
+  for h in ['whiteelo', 'blackelo']:
+    if h in headers:
+      headers[h] = str(pd.to_numeric(headers[h], errors='coerce'))
+  if 'round' in headers:
+    if headers['round'].strip() == '?':
+      headers['round'] = None
+  return headers
+
+def extract_headers_to_columns(in_df, columns_list):
+  out_df = in_df.copy()
+  for col in columns_list:
+    out_df[col] = out_df['headers'].map(lambda x: None if col not in x else x[col])
+  return out_df
+
 def chessgames_to_df(games_list):
-  df = pd.DataFrame(map(lambda g: g.headers, games_list))
-  df.columns = list(map(lambda c: c.strip().lower(), df.columns))
-  df = df.rename({'date': 'eventdate', 'result': 'gameresult'}, axis='columns')
-  df['eventdate'] = df['eventdate'].map(cleanup_event_date)
-  df['eventdate'] = pd.to_datetime(df['eventdate'], errors='coerce')
-  df['pgn'] = list(map(lambda g: str(g.mainline()), games_list))
-  df['whiteelo'] = pd.to_numeric(df['whiteelo'], errors='coerce')
-  df['blackelo'] = pd.to_numeric(df['blackelo'], errors='coerce')
-  df['round'] = pd.to_numeric(df['round'], errors='coerce')
-  col_order = [
+  df = pd.DataFrame()
+  df['headers'] = list(map(lambda g: cleanup_headers(g.headers), games_list))
+  df = extract_headers_to_columns(df, [
     'event',
     'site',
-    'eventdate',
-    'round',
     'white',
     'black',
-    'gameresult',
-    'blackelo',
     'whiteelo',
-    'eco',
-    'pgn'
-  ]
-  return df[col_order]
+    'blackelo',
+    'round',
+    'date',
+    'eventdate',
+    'eco'
+  ])
+  df['eventdate'] = pd.to_datetime(df['eventdate'].fillna(df['date']), errors='coerce')
+  df['whiteelo'] = pd.to_numeric(df['whiteelo'], errors='coerce')
+  df['blackelo'] = pd.to_numeric(df['blackelo'], errors='coerce')
+  del df['date']
+  df['pgn'] = list(map(lambda g: str(g.mainline()), games_list))
+  return df
 
 def load_games_to_db(df, conn):
-  df.to_sql("chessdb", conn, schema='public', if_exists='append', index=False) 
+  try:
+    df.to_sql(
+      "chessdb",
+      conn,
+      schema='public',
+      if_exists='append',
+      index=False,
+      dtype={
+        'event': sqlalchemy.types.String,
+        'site': sqlalchemy.types.String,
+        'white': sqlalchemy.types.String,
+        'black': sqlalchemy.types.String,
+        'whiteelo': sqlalchemy.types.Integer,
+        'blackelo': sqlalchemy.types.Integer,
+        'round': sqlalchemy.types.String,
+        'eventdate': sqlalchemy.types.DATE,
+        'pgn': sqlalchemy.types.String,
+        'eco': sqlalchemy.types.String,
+        'headers': sqlalchemy.types.JSON
+      }
+      )
+  except:
+    pass
 
 if __name__=="__main__":
   conn = generate_psql_conn()
